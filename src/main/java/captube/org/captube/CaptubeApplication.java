4b33168fb3fc1cf586669f14740b9b419b23c374
unknown <jdj9354@gmail.com>
2019-02-27 15:34:28 +0900

    Create spring boot project

---
+++ src/main/java/captube/org/captube/CaptubeApplication.java
@@ -0,0 +1,13 @@
+package captube.org.captube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptubeApplication.class, args);
+    }
+
+}
