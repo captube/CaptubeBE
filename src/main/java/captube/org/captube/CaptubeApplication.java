45626dfe14019e49789aa21aed874a216c1e9bdb
DeokJo Jang <jdj9354@naver.com>
2019-03-03 14:53:59 +0900

    Add pytube df47537aa1588810dd838b984a8db5f1faacfe88 and update gitignore

--- src/main/java/captube/org/captube/CaptubeApplication.java
+++ src/main/java/captube/org/captube/CaptubeApplication.java
@@ -1,13 +1,14 @@
 package captube.org.captube;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ResourceLoader;
 
 @SpringBootApplication
 public class CaptubeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CaptubeApplication.class, args);
     }
-
 }
+
--- src/main/java/captube/org/captube/service/PytubeService.java
+++ src/main/java/captube/org/captube/service/PytubeService.java
@@ -1,20 +1,60 @@
 package captube.org.captube.service;
 
 import captube.org.captube.custom.CaptubeImage;
+import ch.qos.logback.core.util.FileUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.*;
 
 @Service
 public class PytubeService {
 
-    private final String DEFAULT_LANGUAGE = "kor";
 
-    public CaptubeImage[] getImages(String url){
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    private static final String DEFAULT_LANGUAGE = "kor";
+    private static final String PYTUBE_SCRIPT_PATH =  System.getProperty("user.dir") +"/pytube/get_youtube.py";
+    private static final String PYTUBE_RESOURCE_PATH = "pytube";
+    private static final String PYTUBE_COPY_PATH = System.getProperty("user.dir") + "/pytube";
+
+    @PostConstruct
+    public void onPostConstruct() throws IOException {
+        File pytubeFile = new File(PYTUBE_SCRIPT_PATH);
+        if (!pytubeFile.exists()) {
+            File pytubeDir = new File(PYTUBE_COPY_PATH);
+            if (!pytubeDir.exists())
+                pytubeDir.mkdirs();
+            File pytubeResourceFile = new ClassPathResource(PYTUBE_RESOURCE_PATH).getFile();
+            File[] resourceFiles = pytubeResourceFile.listFiles();
+
+            for (File resourceFile : resourceFiles) {
+                InputStream is = new FileInputStream(resourceFile);
+                OutputStream os = new FileOutputStream(new File(PYTUBE_COPY_PATH + File.separator + resourceFile.getName()));
+
+                byte[] buffer = new byte[1024];
+                int read = 0;
+                while ((read = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, read);
+                }
+                is.close();
+                os.close();
+            }
+        }
+
+    }
+
+    public CaptubeImage[] getImages(String url) {
         CaptubeImage[] images = null;
         //Embed pytubeScript
         return images;
     }
 
-    public CaptubeImage[] getImages(String url, String language, boolean isNosub){
+    public CaptubeImage[] getImages(String url, String language, boolean isNosub) {
         CaptubeImage[] images = null;
         //Embed pytubeScript
         return images;
