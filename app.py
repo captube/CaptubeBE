f6a9d49a91f1d8ac00c097be5fa355037f61856e
DeokJo Jang <jdj9354@gmail.com>
2019-09-28 02:16:13 +0900

    REST API transform skeleton code

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+
+@app.route("/api/v1/capture/getImages", methods=['POST'])
+def getImages():
+    jsonRequest = request.json
+
+    url = jsonRequest["url"]
+    responseEncodingType = jsonRequest["responseEncodingType"]
+    language = jsonRequest["language"]
+    isNoSub = jsonRequest["isNoSub"]
+    numberToCapture = jsonRequest["numberToCapture"]
+    startTimeStamp = jsonRequest["startTimeStamp"]
+    endTimeStamp = jsonRequest["endTimeStamp"]
+
+    # TODO Add core logic to capture
+
+    return ""
 
 if __name__ == '__main__':
     app.run()
