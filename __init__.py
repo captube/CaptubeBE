b391da2223bca7d416a2d1a9a3e5489035b90cd5
jang <jang@jangui-Macmini.local>
2020-03-01 19:09:52 +0900

    DynamoDB integration - Archive Item

---
+++ __init__.py
@@ -0,0 +1,4 @@
+import argparse
+
+argsParser = argparse.ArgumentParser()
+argsParser.add_argument('profile', required=False)
--- business/v1/archive.py
+++ business/v1/archive.py
@@ -1,3 +1,12 @@
+from boto3.dynamodb.conditions import Attr
+
+from business import session
+
+dynamodb = session.resource('dynamodb', region_name='ap-northeast-2')
+archiveTable = dynamodb.Table('archive')
+captureItemTable = dynamodb.Table('captureItem')
+
+
 class Archive:
     def getPagedArchive(self, pageKey, pageSize):
         print(f'getPagedArchive, from : {pageKey} to :{pageSize}')
@@ -6,5 +15,23 @@ class Archive:
 
     def getArchive(self, id):
         print(f'getArchive {id}')
-        # TODO Call AWS S3 APIs with boto
-        return {'title': '', 'items': [{'url': '', 'startTime': 0, 'endTime': 0, 'subtitle': ''}]}
+        response = archiveTable.get_item(Key={
+            'id': id
+        })
+
+        archive = archiveTable.get_item(Key={
+            'id': id
+        })['Item']
+        print(f'archive from dynamo : {archive}')
+        captureItems = captureItemTable.scan(
+            FilterExpression=Attr('archiveId').eq(archive['id'])
+        )['Items']
+        print(f'captureItems from dynamo : {len(captureItems)}')
+
+        response = {
+            'title': archive['title'],
+            'items': captureItems
+        }
+        print(f'getArchive response : {response}')
+
+        return response
