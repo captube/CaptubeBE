d2361ad1fa556371e418bded2b170235d392dc68
DeokJo Jang <jdj9354@gmail.com>
2020-03-17 07:34:17 +0900

    Update swagger doc and .gitignore

--- rest/v1/capture.py
+++ rest/v1/capture.py
@@ -1,24 +1,48 @@
 import sys
 import traceback
 
-from flask_restplus import Namespace, reqparse, Resource, marshal, fields
+from flask_restplus import Namespace, Resource, marshal, fields
 
 from business.v1.capture import Capture
 
-parser = reqparse.RequestParser()
 capture = Namespace('capture', description='capture api set')
+
+captureParams = capture.model('CaptureParams', {
+    'url': fields.String(required=True),
+    'language': fields.String(required=False),
+    'numberToCapture': fields.Integer(min=0, required=False),
+    'startTimeStamp': fields.Integer(min=0, required=False),
+    'endTimeStamp': fields.Integer(min=0, required=False)
+})
+
+captureItem = capture.model('captureItems', {
+    'url': fields.String,
+    'startTime': fields.Integer,
+    'endTime': fields.Integer,
+    'subtitle': fields.String
+})
+
+captureResult = capture.model('CaptureResult', {
+    'id': fields.String,
+    'title': fields.String,
+    'captureItems': fields.List(fields.Nested(captureItem))
+})
 
 
 @capture.route('/')
 class GetImages(Resource):
-    def post(self):
-        parser.add_argument('url', required=True, help='url cannot be null or empty', type=str)
-        parser.add_argument('language', required=False, type=str, default="en")
-        parser.add_argument('numberToCapture', required=False, type=int)
-        parser.add_argument('startTimeStamp', required=False, type=int)
-        parser.add_argument('endTimeStamp', required=False, type=int)
+    parser = capture.parser()
 
-        args = parser.parse_args()
+    parser.add_argument('url', required=True, help='url cannot be null or empty', type=str)
+    parser.add_argument('language', required=False, type=str, default="en")
+    parser.add_argument('numberToCapture', required=False, type=int)
+    parser.add_argument('startTimeStamp', required=False, type=int)
+    parser.add_argument('endTimeStamp', required=False, type=int)
+
+    @capture.expect(captureParams)
+    def post(self):
+
+        args = self.parser.parse_args()
         print(f'capture - incoming args {args}')
 
         try:
@@ -34,17 +58,3 @@ class GetImages(Resource):
             traceback.print_exc(file=sys.stdout)
             return 'Internal Server Error', 500
         return marshal(result, captureResult), 200
-
-
-captureItem = capture.model('captureItems', {
-    'url': fields.String,
-    'startTime': fields.Integer,
-    'endTime': fields.Integer,
-    'subtitle': fields.String
-})
-
-captureResult = capture.model('CaptureResult', {
-    'id': fields.String,
-    'title': fields.String,
-    'captureItems': fields.List(fields.Nested(captureItem))
-})
