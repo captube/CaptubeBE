31914e0aa69a4b50b50a18ad2868ab9c8e9fecfd
jang <jdj9354@gmail.com>
2020-06-14 15:40:36 +0900

    Implement endpoint - v2.capture

--- rest/v2/v2.py
+++ rest/v2/v2.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
 from flask_restplus import Api
 
+from rest.v2.capture import capture
+
 blueprint = Blueprint(__name__, '/api/v2', url_prefix='/api/v2')
 api = Api(blueprint, title='api', version='2.0')
+
+api.add_namespace(capture, '/capture')
