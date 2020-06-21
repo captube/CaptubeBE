f4c80b8d19f72363c61a68140fa2c4d192b08e17
jang <jdj9354@gmail.com>
2020-06-21 18:42:13 +0900

    Implement business logic - capture

--- business/v2/capture.py
+++ business/v2/capture.py
@@ -1,7 +1,27 @@
+import shutil
+import uuid
+
+from business import session
+from business.common.CaptureRunner import CaptureRunner
+from business.common.CaptureSaver import CaptureSaver
+from business.common.YoutubeIdParser import YoutubeIdParser
 from core.youtube import youtube
 
 
 class Capture:
+    RESULT_DIR = "results"
+    S3_BUCKET = "captube.captures"
+    S3_PREFIX = "https://s3.ap-northeast-2.amazonaws.com/captube.captures/"
+    dynamodb = session.resource('dynamodb', region_name='ap-northeast-2')
+    archiveTable = dynamodb.Table('archive')
+    captureItemTable = dynamodb.Table('captureItem')
+    # TODO : Need DI
+    youtubeIdParser = YoutubeIdParser()
+    captureRunner = CaptureRunner()
+    captureSaver = CaptureSaver()
+
+    s3_client = session.client('s3')
+
     # youtubeObject is need, because we cannot inject core.youtube.youtube.
     # core.youtube.youtube constructor requires url as parameter
     _youtube = None
@@ -13,3 +33,41 @@ class Capture:
         return {
             "languages": self._youtube.get_available_langs(caption)
         }
+
+    def capture(self, url, language, numberToCapture, startTimeStamp, endTimeStamp):
+        print(f'capture, {url}, {language}, {numberToCapture}, {startTimeStamp}, {endTimeStamp}')
+        id = str(f'{self.youtubeIdParser.parse(url)}_{language}')
+        workingPath = str(uuid.uuid4())
+
+        try:
+            videoInformation = self.captureRunner.capture(url, language, numberToCapture, startTimeStamp, endTimeStamp,
+                                                          workingPath)
+            captureInformation = self._asCaptureInformation(videoInformation, id)
+            self.captureSaver.save(captureInformation)
+        finally:
+            self._clearLocalTemporary(workingPath)
+
+        return captureInformation
+
+    def _asCaptureInformation(self, captureResultByScript, id):
+        result = {
+            "title": captureResultByScript["title"],
+            "thumbnailUrl": captureResultByScript["thumbnail"],
+            "id": id,
+            "captureItems": []
+        }
+
+        frame_infos = captureResultByScript["frame_infos"]
+        for frame_info in frame_infos:
+            result["captureItems"].append({
+                "id": f'{result["id"]}_{frame_info["frame_num"]}',
+                "path": frame_info["img_path"],
+                "timeStamp": frame_info["time_info"],
+                "subtitle": frame_info["script"]
+            })
+
+        return result
+
+    def _clearLocalTemporary(self, id):
+        shutil.rmtree(f'{self.RESULT_DIR}/{id}')
+        return
--- test/business/v2/captureTest.py
+++ test/business/v2/captureTest.py
@@ -1,9 +1,12 @@
 import unittest
+import uuid
+from unittest.mock import MagicMock
+
+from business.v2.capture import Capture
 
 
 class TestCapture(unittest.TestCase):
     def test_getLanguage(self):
-
         # TODO : Cannot test getLanguage, because core.youtube.youtube requires paremeter which is not business logic
         # capture = Capture()
         # url = "someUrl"
@@ -21,3 +24,55 @@ class TestCapture(unittest.TestCase):
         # capture._youtube.get_available_langs.assert_called_with(mockCaption)
         # self.assertEqual(url, result)
         return
+
+    def test_capture(self):
+        videoId = "videoId"
+        url = "url"
+        language = "language"
+        numberToCapture = None
+        startTimerStamp = None
+        endTimeStamp = None
+        woringPath = "woringPath"
+        uuid.uuid4 = MagicMock(return_value=woringPath)
+        capture = Capture()
+        capture.youtubeIdParser.parse = MagicMock(return_value=videoId)
+        capture._clearLocalTemporary = MagicMock(return_value={})
+        videoInformation = {}
+        capture.captureRunner.capture = MagicMock(return_value=videoInformation)
+        captureInformation = {}
+        capture._asCaptureInformation = MagicMock(return_value=captureInformation)
+        capture.captureSaver.save = MagicMock()
+
+        capture.capture(url, language, numberToCapture, startTimerStamp, endTimeStamp)
+
+        capture.captureRunner.capture.assert_called_with(url, language, numberToCapture, startTimerStamp, endTimeStamp,
+                                                         woringPath)
+        capture._asCaptureInformation.assert_called_with(videoInformation, f'{videoId}_{language}')
+        capture.captureSaver.save.assert_called_with(captureInformation)
+        capture._clearLocalTemporary.assert_called()
+
+    def test_capture_when_exception(self):
+        videoId = "videoId"
+        url = "url"
+        language = "language"
+        numberToCapture = None
+        startTimerStamp = None
+        endTimeStamp = None
+        woringPath = "woringPath"
+        uuid.uuid4 = MagicMock(return_value=woringPath)
+        capture = Capture()
+        capture.youtubeIdParser.parse = MagicMock(return_value=videoId)
+        capture._clearLocalTemporary = MagicMock(return_value={})
+        videoInformation = {}
+        capture.captureRunner.capture = MagicMock(return_value=videoInformation)
+        captureInformation = {}
+        capture._asCaptureInformation = MagicMock(return_value=captureInformation)
+        capture.captureSaver.save = MagicMock()
+
+        capture.capture(url, language, numberToCapture, startTimerStamp, endTimeStamp)
+
+        capture.captureRunner.capture.assert_called_with(url, language, numberToCapture, startTimerStamp, endTimeStamp,
+                                                         woringPath)
+        capture._asCaptureInformation.assert_called_with(videoInformation, f'{videoId}_{language}')
+        capture.captureSaver.save.assert_called_with(captureInformation)
+        capture._clearLocalTemporary.assert_called()
